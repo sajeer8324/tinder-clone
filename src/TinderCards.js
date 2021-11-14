1564ccfcb3a7b75c7691cf8c7121c04e3f5db1dc
sajeer8324 <sajeer168324@gmail.com>
2021-11-14 19:59:59 +0530

    yoo

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,19 +1,21 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
+import axios from "./axios";
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name:"Elon Musk",
-            url:"https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg",
-        },
+    const [people, setPeople] = useState([]);
 
-        {
-            name:"Shalu",
-            url:"https://lh3.googleusercontent.com/XQ3B-GtyZdgQMraAjpzApq2hcvjWDhKugxRC2sUEnN6qJH1piR8llQg2kD16Q1VfCP0D8lny1q_PLlsUwUdpRxDHKb6CqEUjbEqCVJzFJEe2h1GU6-Zcf3EpoI4dF-8zpqTwYVJ6V0mHje7AvzO952h04oJOxTmEUAegPR0HKIqXUZAGD--t5Wu6vgF_V7Zmkt2dif2XzM-ohJCBI_q74_DWmz-YNYT5j3WDfOMTFYGpcxs1k9e_cs3qyuvKdgUli2uSYbQSa_lRH6U5Ww88YGS3vC0T3Eo-li70oC7nC-VJemvtO9AdbD0R0E8LpyEKQoMkdRB8cM8WfQW8htWMfR-JQhVLJiLeJmV0QHKg2j67MbYt1DTb3UpOuVdber0dlDSkFErHt_tQRpyh0wlbklupwz1CmEtb7hcRSO0hHmP0IyWQ5jBqz3RNmiorOa6Jhl24F7lrzB525Z5fRY9M8044ku6Uxj7F-bRYTr_xQkf_xDpqWLD2iIMaiKGnhkTyoDpKhrceQawdUQTR3hzYEet5TTNuwi2MJ6WjD8KDWsWSlN-jDn-_LLrg7oZsTVnxB_ToOXVBb-TjjTNsaF1_51kD5dt4Ds7ZEv2DbnIJWEpMeFMyGrhecYOPhv6--9Q_Wnu-Vw0BuXGdX6O-xMjNySo4Tgj-0Clpoc095jGKX31z_Ivv43F-bdtHUxFuAfwY5MCubyRMeW1Cytr-KaJjrAg3=w347-h657-no?authuser=0",
-        },
-    ]);
+    useEffect(() => {
+       async function fetchData() {
+           const req = await axios.get("/tinder/cards");
+
+           setPeople(req.data);
+       }
+
+       fetchData();
+    }, []);
+    console.log(people)
 
     const swiped =(direction, nameToDelete) => {
         console.log("removing:" + nameToDelete);
@@ -36,7 +38,7 @@ function TinderCards() {
                onCardLeftScreen={()=> outOfFrame(person.name)}
                >
                    <div 
-                    style={{backgroundImage:`url(${person.url})`}}
+                    style={{backgroundImage:`url(${person.imgUrl})`}}
                     className="card"
                    >
                     <h3>{person.name}</h3>
